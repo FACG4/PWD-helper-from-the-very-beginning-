edc1a4ce6e47e50463f65cfa036d3651322e3d71
IsraaMoqbel <isramm94@gmail.com>
2018-06-13 14:52:43 +0300

    deleting big comment!!

--- src/controllers/analysis.js
+++ src/controllers/analysis.js
@@ -29,24 +29,3 @@ exports.post = (req, res) => {
     });
   });
 };
-
-// exports.get = (req, res)=>{
-
-//   res.render('fileupload');
-// for uploading images!!!!!!
-//   var form = new formidable.IncomingForm();
-// form.encoding = 'utf-8';
-// form.uploadDir = __dirname + "/images";
-// form.keepExtensions = true;
-//   form.parse(req, function (err, fields, files) {
-// var oldpath = files.filetoupload.path;
-// var newpath = '/home/israa/PWD-helpers/src/controllers/images/' + files.filetoupload.name;
-// fs.rename(oldpath, newpath, function (err) {
-//       if (err) throw err;
-//       res.render('fileupload')
-//     });
-// });
-// res.send(req.body);
-// if(req.body['B.2']==='No'){
-// }
-// }
