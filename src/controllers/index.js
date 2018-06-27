bdadf3e3c7b0e7241b32284eae77a9c8b432fb36
IsraaMoqbel <isramm94@gmail.com>
2018-06-28 00:27:01 +0300

    using multer instead of formidable module to upload multiple images at once!

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const multer = require('multer');
+const upload = multer({
+  dest: 'public/images/'
+});
 
 const router = express.Router();
+
 const home = require('./home');
+const cool = require('./cool');
 const form = require('./form');
 const analysis = require('./analysis');
+const multiUpload = upload.fields([{
+  name: 'img1A',
+  maxCount: 10
+}, {
+  name: 'img1B',
+  maxCount: 10
+}, {
+  name: 'img1D',
+  maxCount: 10
+}, {
+  name: 'img1C',
+  maxCount: 10
+}, {
+  name: 'img1E',
+  maxCount: 10
+}])
 
 router.get('/', home.get);
+router.get('/cool', cool.get);
 router.get('/form', form.get);
-router.post('/analysis', analysis.post);
+router.post('/analysis', multiUpload, analysis.post);
 module.exports = router;
