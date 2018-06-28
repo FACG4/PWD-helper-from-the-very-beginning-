8e3d775046ec6b515967ebace470e4dd24712f34
IsraaMoqbel <isramm94@gmail.com>
2018-06-28 12:35:59 +0300

    adding some abstraction and send analysis to analysis page

--- src/controllers/analysis.js
+++ src/controllers/analysis.js
@@ -5,79 +5,64 @@ const JSZip = require('jszip');
 const ImageModule = require('docxtemplater-image-module');
 
 let solutions = [];
-const solutionA1 = ' you have to do something for the entrance';
-const solutionA2 = ' you have to do something for the entrance for A2 in checklist';
+const solutionA1 = 'The entrance of the building should be located at less than 50m from the street.';
+const solutionA2 = 'The entrance of the building located at less than 50m from the parking.';
+const solutionA3_1 = 'There should be enough parking bays reserved for people with disabilities.';
+const solutionA3_2 = 'The reserved parking bay should be located at less than 50m to the building entrance.';
 
 exports.post = (req, res) => {
-  let inputsValues=req.body;
+  let inputsValues = req.body;
+  const MultipleImages = ['img1A', 'img1B', 'img1C', 'img1D', 'img1E'];
+  let imagesPaths = {};
 
-  console.log('req.body:',req.body);
-  if(req.files.img1A) {
-    for(let i=0; i<req.files.img1A.length; i++){
-      inputsValues[`img1A${i}`]=req.files.img1A[i].path;
+  MultipleImages.forEach((element, index) => {
+    if (req.files[element]) {
+      for (let x = 0; x < req.files[element].length; x++) {
+        inputsValues[`${element}${x}`] = req.files[element][x].path;
+        imagesPaths[`${element}${x}`] = req.files[element][x].path.substring(7, );
+      }
     }
+  });
+
+  const content = fs
+    .readFileSync(path.join(__dirname, 'template.docx'), 'binary');
+  const opts = {};
+  opts.centered = false;
+  opts.getImage = function(tagValue, tagName) {
+    return fs.readFileSync(tagValue);
+  };
+
+  opts.getSize = function(img, tagValue, tagName) {
+    return [200, 200];
+  };
+
+  const imageModule = new ImageModule(opts);
+  const zip = new JSZip(content);
+  const doc = new Docxtemplater().attachModule(imageModule).loadZip(zip).setData(inputsValues)
+    .render();
+
+  try {
+    doc.render();
+  } catch (err) {
+    return res.status(500).json({
+      failed: true,
+    });
   }
-  if (req.files.img1B) {
-    for(let i=0; i<req.files.img1B.length; i++){
-      inputsValues[`img1B${i}`]=req.files.img1B[i].path;
-    }
-  }
-  console.log(inputsValues);
-  if (req.files.img1C) {
-    for(let i=0; i<req.files.img1C.length; i++){
-      inputsValues[`img1C${i}`]=req.files.img1C[i].path;
-    }
-  }
-  if (req.files.img1D) {
-    for(let i=0; i<req.files.img1D.length; i++){
-      inputsValues[`img1D${i}`]=req.files.img1D[i].path;
-    }
-  }
-  if (req.files.img1E) {
-    for(let i=0; i<req.files.img1E.length; i++){
-      inputsValues[`img1E${i}`]=req.files.img1E[i].path;
-    }
-  }
 
-    const content = fs
-      .readFileSync(path.join(__dirname, 'template.docx'), 'binary');
-    const opts = {};
-    opts.centered = false;
-    opts.getImage = function(tagValue, tagName) {
-      return fs.readFileSync(tagValue);
-    };
-
-    opts.getSize = function(img, tagValue, tagName) {
-      return [200, 200];
-    };
-
-    const imageModule = new ImageModule(opts);
-    const zip = new JSZip(content);
-    const doc = new Docxtemplater().attachModule(imageModule).loadZip(zip).setData(inputsValues)
-      .render();
-
-    try {
-      doc.render();
-    } catch (err) {
-      return res.status(500).json({
-        failed: true,
-      });
-    }
-
-    const buf = doc.getZip()
-      .generate({
-        type: 'nodebuffer',
-      });
-
-    const filePath = path.join(__dirname, '..', '..', 'public', 'form.docx');
-
-    fs.writeFileSync(filePath, buf);
-
-    res.render('analysis', {
-
-      status: true,
+  const buf = doc.getZip()
+    .generate({
+      type: 'nodebuffer',
     });
 
-    solutions = [];
+  const filePath = path.join(__dirname, '..', '..', 'public', 'form.docx');
+
+  fs.writeFileSync(filePath, buf);
+
+  res.render('analysis', {
+    imagesPaths
+
+  });
+
+  solutions = [];
 
 };
