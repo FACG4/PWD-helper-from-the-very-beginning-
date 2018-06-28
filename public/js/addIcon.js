ec3c83c405e445457a0a17345bcd6951c1a617c9
InassTubail <anoos.hanii@gmail.com>
2018-06-28 12:43:43 +0300

    update style

--- public/js/addIcon.js
+++ public/js/addIcon.js
@@ -1,39 +1,7 @@
-const select = element => document.getElementById(element);
-const nextToB = select('nextToB');
-const nextToD = select('nextToD');
-const nextToC = select('nextToC');
-const previousToA = select('previousToA');
-const previousToB = select('previousToB');
-const previousToD = select('previousToD');
-const previousToC = select('previousToC');
+let preventdef = document.getElementsByClassName('prevent');
 
-previousToA.addEventListener("click", function (e) {
-    e.preventDefault();
-});
-
-previousToB.addEventListener("click", function (e) {
-    e.preventDefault();
-});
-previousToD.addEventListener("click", function (e) {
-    e.preventDefault();
-});
-previousToC.addEventListener("click", function (e) {
-    e.preventDefault();
-});
-
-nextToB.addEventListener("click", function (e) {
-    e.preventDefault();
-});
-
-nextToD.addEventListener("click", function (e) {
-    e.preventDefault();
-});
-
-nextToC.addEventListener("click", function (e) {
-    e.preventDefault();
-});
-
-const nextToE = document.getElementById('nextToE');
-nextToE.addEventListener("click", function (e) {
-    e.preventDefault();
-});
+for (var i = 0; i < preventdef.length; i++) {
+    preventdef[i].addEventListener("click", function (e) {
+        e.preventDefault();
+    });
+}
